59df09c3b0c662a28c02a8c6fd45969360bdd162
MATTOP\Matthew <m.durkin92@gmail.com>
2021-03-25 15:38:21 +0000

    Add more content including comparison grid of current centralised/decentralised

--- src/ReactContent.tsx
+++ src/ReactContent.tsx
@@ -5,29 +5,78 @@ export const ReactContent = () => {
   return (
     <Container>
       <TransparentSection>
-        <Header>Transparent content moderation powered by Chainlink</Header>
+        <Heading>Transparent content moderation - powered by Chainlink</Heading>
       </TransparentSection>
       <WhiteSection>
-        <Header>Transparent content moderation powered by Chainlink</Header>
-        <Header>Transparent content moderation powered by Chainlink</Header>
-        <Header>Transparent content moderation powered by Chainlink</Header>
-        <Header>Transparent content moderation powered by Chainlink</Header>
-        <Header>Transparent content moderation powered by Chainlink</Header>
-        <Header>Transparent content moderation powered by Chainlink</Header>
-        <Header>Transparent content moderation powered by Chainlink</Header>
-        <Header>Transparent content moderation powered by Chainlink</Header>
-        <Header>Transparent content moderation powered by Chainlink</Header>
-        <Header>Transparent content moderation powered by Chainlink</Header>
-        <Header>Transparent content moderation powered by Chainlink</Header>
-        <Header>Transparent content moderation powered by Chainlink</Header>
-        <Header>Transparent content moderation powered by Chainlink</Header>
-        <Header>Transparent content moderation powered by Chainlink</Header>
-        <Header>Transparent content moderation powered by Chainlink</Header>
-        <Header>Transparent content moderation powered by Chainlink</Header>
-        <Header>Transparent content moderation powered by Chainlink</Header>
-        <Header>Transparent content moderation powered by Chainlink</Header>
-        <Header>Transparent content moderation powered by Chainlink</Header>
-        <Header>Transparent content moderation powered by Chainlink</Header>
+        <Subheading>
+          Content moderation should be focused on protecting the safety of young
+          or otherwise vulnerable users
+        </Subheading>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </p>
+        <Subheading>
+          Today users have two choices when it comes to social networks...
+        </Subheading>
+        <ComparisonGrid>
+          <ComparisonGridCol>
+            <ComparisonGridColHeading>
+              Typical centralised
+            </ComparisonGridColHeading>
+            <ul>
+              <BadLi>
+                <b>Users are the product - </b>User data may be unknowingly sold
+                to advertisers for profit.
+              </BadLi>
+              <BadLi>
+                <b>Risk of bias - </b>Application has complete control over what
+                content users are shown.
+              </BadLi>
+              <MedLi>
+                <b>Opaque content moderation - </b>Vulnerable users are for the
+                most part protected from malicious content, but any user
+                generated content may be removed with no explanation. Platform
+                bias may influence moderation.
+              </MedLi>
+            </ul>
+          </ComparisonGridCol>
+          <ComparisonGridCol>
+            <ComparisonGridColHeading>
+              Typical decentralised
+            </ComparisonGridColHeading>
+            <ul>
+              <GoodLi>
+                <b>Users own their own data</b>
+              </GoodLi>
+              <MedLi>
+                <b>No platform-level content moderation - </b>Onus of content
+                moderation is placed solely on the users. This eliminates any
+                platform bias but introduces a high risk of vulnerable users
+                being exposed to malicious content before other users have
+                chance to flag it.
+              </MedLi>
+            </ul>
+          </ComparisonGridCol>
+        </ComparisonGrid>
+        <Quote>
+          I think the issue of moderation is extremely difficult, and you’re
+          seeing a lot of centralized companies today struggle with that.
+          There’s massive overhead in hiring people, and those people often get
+          PTSD [post traumatic stress disorder] because they’re looking at a lot
+          of the same graphic things over and over and over again. - Eric Yang,
+          founder and executive director of{" "}
+          <QuoteLink href="https://junto.foundation/">Junto</QuoteLink> -{" "}
+          <QuoteSource href="https://cointelegraph.com/news/social-media-problems-and-solutions-centralization-is-not-sustainable">
+            Source
+          </QuoteSource>
+        </Quote>
+        <VideoPlaceholder>Video placeholder</VideoPlaceholder>
       </WhiteSection>
     </Container>
   );
@@ -60,11 +109,113 @@ const TransparentSection = styled.div`
 `;
 
 const WhiteSection = styled.div`
-  padding: 100px;
+  padding: 50px 100px;
   position: relative;
+  max-width: 800px;
   min-height: calc(100vh - 500px);
   background-color: white;
   color: black;
+  border-radius: 4px;
+
+  @media (max-width: 1024px) {
+    padding: 50px;
+  }
+
+  @media (max-width: 768px) {
+    padding: 20px;
+  }
 `;
 
-const Header = styled.h1``;
+const Heading = styled.h1`
+  text-align: center;
+  font-size: 2.6rem;
+  font-weight: 400;
+  font-family: "Fredoka One", cursive;
+
+  @media (max-width: 768px) {
+    font-size: 1.4rem;
+  }
+`;
+
+const Subheading = styled.h2`
+  font-size: 2rem;
+  font-weight: 400;
+  font-family: "Fredoka One", cursive;
+  color: #6c02c9;
+
+  @media (max-width: 768px) {
+    font-size: 1.2rem;
+  }
+`;
+
+const ComparisonGrid = styled.div`
+  display: grid;
+  grid-template-columns: 50% 50%;
+  grid-template-rows: auto;
+  gap: 20px;
+
+  @media (max-width: 768px) {
+    grid-template-columns: 100%;
+    grid-template-rows: auto auto;
+  }
+`;
+
+const ComparisonGridCol = styled.div`
+  min-height: 200px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: flex-start;
+`;
+
+const ComparisonGridColHeading = styled.span`
+  font-size: 1.4rem;
+  text-decoration: underline;
+
+  @media (max-width: 768px) {
+    font-size: 1.2rem;
+  }
+`;
+
+const Quote = styled.div`
+  font-style: italic;
+  background-color: #a4eafc;
+  color: #003f9e;
+  font-size: 1.2rem;
+  padding: 20px;
+  margin: 20px;
+  border-radius: 8px;
+`;
+
+const QuoteLink = styled.a`
+  pointer-events: auto;
+  color: #003f9e;
+`;
+
+const QuoteSource = styled(QuoteLink)`
+  font-style: initial;
+  font-size: 0.8rem;
+  color: #003f9e;
+`;
+
+const BadLi = styled.li`
+  color: red;
+`;
+
+const MedLi = styled.li`
+  color: orange;
+`;
+
+const GoodLi = styled.li`
+  color: green;
+`;
+
+const Typography = styled.span``;
+
+const VideoPlaceholder = styled.div`
+  width: 100%;
+  max-width: 800px;
+  height: 450px;
+  background-color: red;
+  margin: 20px 0;
+`;
